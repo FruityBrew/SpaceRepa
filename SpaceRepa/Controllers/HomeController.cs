d2c88dc27e1686fe69e8ada95718b74d4c20f7a4
VovaKombarov <vovakom1988@gmail.com>
2018-08-02 09:29:45 +0300

    #Test Commit 1

--- SpaceRepa/Controllers/HomeController.cs
+++ SpaceRepa/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace SpaceRepa.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+              ViewData["Message"] = "Your application description page.";   
 
             return View();
         }
