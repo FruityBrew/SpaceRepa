500cc843272837bfa66fb28d6692b2515b296e13
FruityBrew <kompota@yandex.ru>
2018-08-23 11:18:18 +0300

    #реглам

--- IdentityDb.API/Startup.cs
+++ IdentityDb.API/Startup.cs
@@ -23,15 +23,21 @@ namespace IdentityDb.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method
+        // to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            String connection = Configuration.GetConnectionString("AzureConnection");
-            services.AddDbContext<IdentityContext>(options => options.UseSqlServer(connection));
+            String connection = 
+                Configuration.GetConnectionString("AzureConnection");
+
+            services.AddDbContext<IdentityContext>(
+                options => options.UseSqlServer(connection));
+
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. 
+        //Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
